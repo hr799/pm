8e05c9c2e5946ae0f29124f7da2c64d9cb4342a2
hr799 <hr799@hotmail.com>
2017-01-13 18:27:12 +1300

    updated project detail page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,6 +15,15 @@ function ($scope, $stateParams) {
 
 
 }])
+
+//add new controller.
+.controller('projectDetailCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
    
 .controller('feedbackCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
