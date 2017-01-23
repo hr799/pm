170bdf1f5aa905265e9715dfe6d543a73cbabe07
hr799 <hr799@hotmail.com>
2017-01-23 20:14:08 +1300

    v2.1 Add data validation to login page
    1. user email validation
    2. user email format validation
    3. user password validation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -160,11 +160,12 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', ['$scope', '$stateParams', '$ionicPopup',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicPopup) {
      $scope.user = {email:"", psw:""};
+
      $scope.login = function() {
         firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.psw).then(function(msg) {
             // Login Success
@@ -172,11 +173,34 @@ function ($scope, $stateParams) {
             location.href = "#tab/home";
         }, function(error){
             console.log(error);
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if(errorCode=="auth/user-not-found"){
+                $ionicPopup.alert({
+                    title:'Invalid email',
+                    template:'User not found'
+                })
+            }else if(errorCode=="auth/wrong-password"){
+                $ionicPopup.alert({
+                    title:'invalid Password',
+                    template:'The password is Invalid'
+                })
+            }else if(errorCode=="auth/invalid-email"){
+                $ionicPopup.alert({
+                    title:'format error',
+                    template:'The password address is badly formatted'
+                })
+            }
         });
 
     };
 
 }])
+
+
+
+
+
    
 .controller('signUpCtrl', ['$scope', '$stateParams', '$ionicPopup',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -189,18 +213,18 @@ function ($scope, $stateParams, $ionicPopup) {
         var psw = $scope.user.psw;
 
             if(psw.length < 6){
-                var alertPopup = $ionicPopup.alert({
+                $ionicPopup.alert({
                     title:'Weak Password',
                     template:'Password should not be less than 6 characters'
                 })
             }else if(psw.length >25){
-                var alertPopup = $ionicPopup.alert({
+                $ionicPopup.alert({
                     title:'Invalid Password',
                     template:'Password should not exceed 25 characters'
                 })
 
             }else if(psw.search(/[A-Z]/) <0){
-                var alertPopup = $ionicPopup.alert({
+                $ionicPopup.alert({
                     title:'Invalid Password',
                     template:'Password should contain at lease 1 uppercase'
                 })
@@ -216,12 +240,12 @@ function ($scope, $stateParams, $ionicPopup) {
                   var errorCode = error.code;
                   var errorMessage = error.message;
                     if(errorCode=='auth/email-already-in-use'){
-                        var alertPopup = $ionicPopup.alert({
+                        $ionicPopup.alert({
                         title:'Invalid email',
                         template:'This email address is already in use'
                         })
                     }else if(errorCode=="auth/invalid-email"){
-                        var alertPopup = $ionicPopup.alert({
+                        $ionicPopup.alert({
                         title:'Invalid email',
                         template:'The email address is badly formatted'
                         })
