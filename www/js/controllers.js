fe98aac12f35e89488ed39bd8e5304e408903594
hr799 <hr799@hotmail.com>
2017-02-27 00:30:40 +1300

    save new project detail to firebase database successful

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -299,15 +299,20 @@ function ($scope, $stateParams) {
 }])
 
 //add new controller.
-.controller('addProjectCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('addProjectCtrl', ['$scope', '$stateParams','$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicPopup) {
     $scope.project = {};
     $scope.saveProject = function(){
         var userId = firebase.auth().currentUser.uid;
-        firebase.database().ref('users/' + userId + '/projects').push($scope.project).then(function(res){
-            
+        $scope.project.startDate = $scope.project.startDate.toString();
+        var newProjectRef = firebase.database().ref('users/' + userId + '/projects').push();
+        newProjectRef.set($scope.project).then(function(res){
+            $ionicPopup.alert({
+                    
+                    template:'New Project Created'
+                })
             location.href="#/tab/project";
         });
     
