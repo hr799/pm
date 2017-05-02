c1a05b02e7076d69652466cdf8c00ae41ce267ef
Aaron He <hr799@hotmail.com>
2017-05-02 23:31:09 +1200

    fixed gesture conflict - banner & sidemenu

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 
 })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $ionicSideMenuDelegate) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -26,6 +26,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    $ionicSideMenuDelegate.canDragContent(false);
   });
 })
 
